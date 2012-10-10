2aec6c2a146841c02f85780e68cddafd448a0a4d
Advtech <daniel.sapelli@gmail.com>
2012-10-10 13:56:53 -0400

    Fixed the hammer a bit
    Adding new Blocks/Items for nether survival

---
+++ src/common/advtech/mods/DarkShadows/Buildings/TunnelMaker.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package advtech.mods.DarkShadows.Buildings;
+
+import net.minecraft.src.Block;
+import net.minecraft.src.Material;
+
+/**
+ * @author advtech92
+ *
+ */
+public class TunnelMaker extends Block {
+	public TunnelMaker(int tunnelMakerID, int spriteID, Material material){
+		super(tunnelMakerID, spriteID, material);
+	}
+
+}
